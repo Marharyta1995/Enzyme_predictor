0416cf599d67c1a1718c33547a56c13294ed9566
Marharyta1995 <m.varatnitskaya@gmail.com>
2023-01-15 17:17:46 +0100

    Model injection with depends was included

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import torch
 
 from model.cnn_model import initilize_model
@@ -14,23 +14,24 @@ app = FastAPI(
 )
 
 
-@app.post('/predict', tags= ["Single sequence"], description= "Make your prediction based on amino acid sequence")
-def do_predict(body: AA_Sequence):
-
+async def load_model():
     # Initialize the pytorch model
     cnn_model, optimizer = initilize_model(vocab_size=26,
-                                       embed_dim=50,
-                                       learning_rate=0.2,
-                                       dropout=0.5)
+                                           embed_dim=50,
+                                           learning_rate=0.2,
+                                           dropout=0.5)
 
     cnn_model.load_state_dict(torch.load("model/model.pt"))
     cnn_model.eval()
 
-    # prepare input data
-    #X = body.sequence
+    return cnn_model
+
+
+@app.post('/predict', tags=["Single sequence"], description="Make your prediction based on amino acid sequence")
+async def do_predict(body: AA_Sequence, model = Depends(load_model)):
 
     # run model inference
-    probs = predict(body.sequence, model=cnn_model)
+    probs = predict(body.sequence, model=model)
     enzyme_result = np.argmax(probs)
 
     is_enzyme = {0: "not_enzyme", 1: "enzyme"}
